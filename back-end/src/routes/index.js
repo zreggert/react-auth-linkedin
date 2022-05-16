4e0285ff1d40c5d40ec8331a6e5911474f5a1a75
elgalloloco <zreggert@gmail.com>
2022-05-16 16:32:12 -0400

    added login routes

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,8 +1,9 @@
 import { signUpRoute} from './signUpRoute';
-
+import { loginRoute } from './loginRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
+    loginRoute,
     signUpRoute,
     testRoute,
 ];
--- back-end/src/routes/signUpRoute.js
+++ back-end/src/routes/signUpRoute.js
@@ -1,6 +1,7 @@
-import { getDbConnection } from '../db';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { getDbConnection } from '../db';
+
 
 export const signUpRoute = {
     path: '/api/signup',
@@ -13,42 +14,44 @@ export const signUpRoute = {
 
         if (user) {
             res.sendStatus(409);
+        } else {
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const startingInfo = {
+                hairColor: '',
+                favoriteFood: '',
+                bio: '',
+            };
+
+            const result = await db.collection('users').insertOne({
+                email,
+                passwordHash,
+                info: startingInfo,
+                isVerified: false,
+            });
+
+            const { insertedId } = result;
+            
+            jwt.sign({
+                id: insertedId,
+                email,
+                info: startingInfo,
+                isVerified: false,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '2d',
+            },
+            (err, token) => {
+                if (err) {
+                    return res.status(500).send(err);
+                } else {
+                    res.status(200).json({ token });
+                }
+                
+            })
         }
 
-        const passwordHash = await bcrypt.hash(password, 10);
-
-        const startingInfo = {
-            hairColor: '',
-            favoriteFood: '',
-            bio: '',
-        };
-
-        const result = await db.colllection('users').insertOne({
-            email,
-            passwordHash,
-            info: startingInfo,
-            isVerified: false,
-        });
-
-        const { insertedId } = result;
-        
-        jwt.sign({
-            id: insertedId,
-            email,
-            info: startingInfo,
-            isVerified: false,
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '2d',
-        },
-        (err, token) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            res.status(200).json({ token });
-        }
-        )
     }
 
 }
